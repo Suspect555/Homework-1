dae8a753bb92bf44a3ff5ac9a1a5dd3678b3fc44
Sergey <suspect555list.ru>
2023-04-06 01:36:02 +0300

    Task_13 Solved

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -14,33 +14,31 @@ if(num>=100 & num <=1000) //3
 {
     int lastDigit = num % 10;
     
-
-Console.WriteLine($"{num}");
-Console.WriteLine($"{lastDigit}");
+    Console.WriteLine($"{num} -> {lastDigit}");
 }
 if(num>=1000 & num <=10000) //4
 {
     int firstDigit = num % 100;
     int lastDigit = firstDigit / 10;
 
-    Console.WriteLine($"{num}");
-    Console.WriteLine($"{lastDigit}");
+    Console.WriteLine($"{num} -> {lastDigit}");
 }
 if(num>=10000 & num <=100000) //5
 {
     int firstDigit = num % 1000;
     int lastDigit = firstDigit / 100;
 
-    Console.WriteLine($"{num}");
-    Console.WriteLine($"{lastDigit}");
+    Console.WriteLine($"{num} -> {lastDigit}");
 }
 if(num>=100000 & num <=1000000) //6
 {
     int firstDigit = num % 10000;
     int lastDigit = firstDigit / 1000;
 
-    Console.WriteLine($"{num}");
-    Console.WriteLine($"{lastDigit}");
+    Console.WriteLine($"{num} -> {lastDigit}");
 }
-
+if (num<100)
+{
+    Console.WriteLine($"{num} -> Третьей цифры нет");
+}
 
