866476236069b976b195094d5be5a6f9993d0d01
Sergey <suspect555list.ru>
2023-04-29 18:41:29 +0300

    Task_52 solved

--- Task_52/Program.cs
+++ Task_52/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10); 
+        }
+    }
+}
+
+int SummOfMainDiagonal(int[,] matr)
+{
+ int result = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (i==j)
+            {
+                result += matr[i,j];
+                
+            }
+        }
+    }
+ return result;
+    
+}
+int x = 3;
+int y = 4;
+int[,] matrix = new int[x, y];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+SummOfMainDiagonal(matrix);
+Console.WriteLine();
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        avarage = (avarage + matrix[i, j]);
+    }
+    avarage = avarage / x;
+    Console.Write(Math.Round(avarage, 2) + "; ");
+}
+
