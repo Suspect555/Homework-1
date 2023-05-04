901284e551aab74ebf665ca5c4eea50bd0b034cb
Sergey <suspect555list.ru>
2023-05-04 16:57:30 +0300

    Task_68 solved

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -3,19 +3,18 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
- 
+
 Console.WriteLine("Enter m Number");
 int m = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter n Number");
 int n = int.Parse(Console.ReadLine());
 
-int32 result = AkkermanFunction(m,n);
-AkkermanFunction(m,n);
+int result = AkkermanFunction(m, n);
+AkkermanFunction(m, n);
 Console.WriteLine($"M = {m}; N = {n} -> A(m,n) = {result}");
 
 int AkkermanFunction(int m, int n)
 {
-    
     if (m == 0)
     {
         return n + 1;
@@ -28,5 +27,4 @@ int AkkermanFunction(int m, int n)
     {
         return (AkkermanFunction(m - 1, AkkermanFunction(m, n - 1)));
     }
-    
 }
